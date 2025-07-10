6acb9037de8bb1672ee7b0e9604b037f9f5a6878
ramashashank2003 <ramashashank2003@gmail.com>
2025-07-10 22:48:59 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0d03cb826412c6b0f"
   instance_type = "t2.micro"
-  key_name      = "devopsbyraham"
+  key_name      = "rama192619"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
